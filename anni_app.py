8d14489e8506658f9c22ae0269542bcce279a7c7
thedigitalraft <thedigitalraft@gmail.com>
2026-04-07 21:59:26 -0600

    Add files via upload

--- anni_app.py
+++ anni_app.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 
 # ── CONFIGURACIÓN ─────────────────────────────────────────────────────────────
 
-ANNI_VERSION = "1.0.68"
+ANNI_VERSION = "1.0.69"
 ANNI_CREDITS = "ANNI — creada por Rafa Torrijos"
 
 TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY", "")
